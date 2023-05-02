990b88f64d4d52733be0331f8e1ac97e933656e7
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-05-02 09:55:19 +0200

    edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -21,5 +25,15 @@ class ArticlesController < ApplicationController
     end
     # display error messages due to the validations
     # Validations and display messages
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was created successfully :)"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
