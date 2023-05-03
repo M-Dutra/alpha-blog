5d99e1182b5443849f90ca26f761451205ae17fb
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-05-03 16:50:41 +0200

    Errors and messages styling - alerts, bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Remove errors setting that rails add by defaul:
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
