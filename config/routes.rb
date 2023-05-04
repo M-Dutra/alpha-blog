efaa0e73ea1bad1f148af797e7627c06697d8923
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-05-04 15:07:57 +0200

    users form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   #Rest-ful routes to Rails resources
   #HTTP verbs - Get, Post, Put, Patch, Delete
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
